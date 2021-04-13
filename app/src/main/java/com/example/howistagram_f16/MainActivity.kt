f3e5cd6b12d562687141d4668ec8622f8584f3ce
cheonjiwan <giyan1000@naver.com>
2021-04-13 21:03:27 +0900

    사진 업로드 페이지 및 컨텐츠 데이터 모델

--- app/src/main/java/com/example/howistagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howistagram_f16/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.howistagram_f16
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.howistagram_f16.navigation.AlarmFragment
-import com.example.howistagram_f16.navigation.DetailViewFragment
-import com.example.howistagram_f16.navigation.GridFragment
-import com.example.howistagram_f16.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.howistagram_f16.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
     override fun onNavigationItemSelected(Item: MenuItem): Boolean {
@@ -28,6 +30,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
@@ -48,6 +54,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
 
